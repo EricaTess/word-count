32fea108308b70ed75414d205808d39f8930fcf4
Erica Sanchez <sanchez.ericat@gmail.com>
2020-05-04 19:45:56 +0000

    Completed word count that prints each word and number of instances

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,20 @@
 # put your code here.
+import sys
+
+def count_words(phrase):
+    # print(phrase)
+
+    file = open(sys.argv[1])
+
+    word_count = {}
+
+    for line in file:
+        lines = line.rstrip('\r\n').split(' ')
+
+        for word in lines:
+            word_count[word] = word_count.get(word, 0) + 1
+
+    for word, num in word_count.items():
+        print(f'{word} {num}')
+
+count_words(sys.argv)
